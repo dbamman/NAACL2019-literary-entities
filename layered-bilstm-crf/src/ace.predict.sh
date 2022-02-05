1e93f6abb4ede6229f7aaf21ee5feb8fc0c8d81a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-05 14:03:26 +0800

    Delete layered-bilstm-crf directory

--- layered-bilstm-crf/src/ace.predict.sh
+++
@@ -1,2 +0,0 @@
-for i in `ls /data/dbamman/naacl2019/gutenberg/transformed`; do basen=`basename $i`; echo $basen; echo $i; python3 predict.py ace.lit.test.config /data/dbamman/naacl2019/gutenberg/transformed/$basen /data/dbamman/naacl2019/gutenberg/ace_predict/$basen; done >> ace.predict.log 2>&1
-
